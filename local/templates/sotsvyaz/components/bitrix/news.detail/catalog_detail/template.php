c8df8d0d538a1b039c723d2572bac7b6deb35634
iamalexneumann <iamalexneumann@yandex.ru>
2023-06-21 04:55:01 +0300

    Adds the base catalog template

---
+++ local/templates/sotsvyaz/components/bitrix/news.detail/catalog_detail/template.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+/**
+ * @var array $arParams
+ * @var array $arResult
+ * @global CMain $APPLICATION
+ * @global CUser $USER
+ * @global CDatabase $DB
+ * @var array $arLangMessages
+ * @var CBitrixComponentTemplate $this
+ * @var string $templateName
+ * @var string $templateFile
+ * @var string $templateFolder
+ * @var string $parentTemplateFolder
+ * @var string $componentPath
+ * @var array $templateData
+ * @var CBitrixComponent $component
+ */
+$this->setFrameMode(true);
+use Bitrix\Main\Localization\Loc;
+?>
+<div class="news-detail">
+    <?= $arResult['NAME']; ?>
+</div>
