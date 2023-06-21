182969f0987a748305a7137bdfe382f7900cbd7e
iamalexneumann <iamalexneumann@yandex.ru>
2023-06-21 16:51:43 +0300

    Adds the main map

---
+++ blog/.sitemap_submenu.menu_ext.php
@@ -0,0 +1,32 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+    die();
+
+global $APPLICATION;
+
+if (CModule::IncludeModule("iblock")) {
+    $arSelect = Array("NAME", "CODE");
+    $arFilter = Array("IBLOCK_ID" => 5, "ACTIVE_DATE" => "Y", "ACTIVE" => "Y");
+
+    $elements = [];
+
+    $res = CIBlockElement::GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
+
+    while($ob_arr = $res->Fetch()) {
+        $elements[] = $ob_arr;
+    }
+
+    $aMenuLinksExt = [];
+
+    foreach ($elements as $element) {
+        $aMenuLinksExt[] = [
+            $element["NAME"],
+            "/blog/" . $element["CODE"] . "/",
+            [],
+            [],
+            ""
+        ];
+    }
+
+    $aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
+}
--- sitemap/index.php
+++ sitemap/index.php
@@ -3,6 +3,20 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Карта сайта");
 ?>
 
-Text here....
+<?php $APPLICATION->IncludeComponent(
+    "bitrix:main.map",
+    "main_map",
+    Array(
+        "CACHE_TIME" => "3600",
+        "CACHE_TYPE" => "A",
+        "COL_NUM" => "1",
+        "COMPONENT_TEMPLATE" => ".default",
+        "COMPOSITE_FRAME_MODE" => "A",
+        "COMPOSITE_FRAME_TYPE" => "AUTO",
+        "LEVEL" => "10",
+        "SET_TITLE" => "N",
+        "SHOW_DESCRIPTION" => "Y"
+    )
+); ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
